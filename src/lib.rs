eb72269994060ba86a86b2fc028b718450632a9f
curtis-wils0n <curtis.wils0n@protonmail.com>
2024-01-17 10:37:08 -0700

    Include basic interrupt handling and init function

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,24 @@
-#![no_std]
 #![cfg_attr(test, no_main)]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![feature(abi_x86_interrupt)]
+#![no_std]
 #![reexport_test_harness_main = "test_main"]
+#![test_runner(crate::test_runner)]
 
 use core::panic::PanicInfo;
 pub mod serial;
 pub mod vga_buffer;
+pub mod interrupts;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u32)]
 pub enum QemuExitCode {
   Success = 0x10,
   Failed = 0x11,
+}
+
+pub fn init() {
+  interrupts::init_idt();
 }
 
 pub fn exit_qemu(exit_code: QemuExitCode) {
@@ -56,6 +62,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+  init();
   test_main();
   loop {}
 }
